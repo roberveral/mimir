303614874c41e3d00fefb1e7bf3a59cc2570bbb1
roberto.veral <roberto.veral@datiobbva.com>
2019-06-13 11:09:49 +0200

    [FIX] Fix OAuth configuration variables: authorization endpoint is in the UI, not the API backend

--- config/oauth.go
+++ config/oauth.go
@@ -17,25 +17,30 @@ type OAuth struct {
 	// AccessTokenTTL is the time of expiration of new access tokens, as a duration string.
 	// Example: 10s, 3h
 	// Default value is 3h
-	AccessTokenTTL TTL `default:"3h"`
+	AccessTokenTTL TTL `split_words:"true" default:"3h"`
 	// AuthorizationCodeTTL is the time of expiration of new authorization codes, as a duration string.
 	// Example: 10s, 3h
 	// Default value is 30s
-	AuthorizationCodeTTL TTL `default:"30s"`
+	AuthorizationCodeTTL TTL `split_words:"true" default:"30s"`
 	// Issuer is the issuer set in access tokens.
 	// Default value is: "localhost:8000"
 	Issuer string `default:"http://localhost:8000"`
 	// ScopesSupported is the list of OAuth scopes that the server supports,
 	// without including the OpenID related ones.
-	ScopesSupported []string `default:""`
+	ScopesSupported []string `split_words:"true" default:""`
+	// UIAuthorizeEndpoint is the URL for the authorize endpoint in the UI which finishes the OAuth 2.0
+	// implementation using this Mimir API as backend.
+	// Default value is: "http://localhost:4200/oauth/authorize"
+	UIAuthorizeEndpoint string `default:"http://localhost:4200/oauth/authorize"`
 }
 
 // Manager instantiates a new OAuth manager based on the configuration.
 // It uses the given identity provider, store and token provider.
 func (c *OAuth) Manager(idp idp.IdentityProvider, store *mongodb.Store, encoder jwt.Encoder) *oauth.Manager {
+	fmt.Println(c)
 	providerMetadata := &openid.ProviderMetadata{
 		Issuer:                            c.Issuer,
-		AuthorizationEndpoint:             fmt.Sprintf("%s/v0/oauth/authorize", c.Issuer),
+		AuthorizationEndpoint:             c.UIAuthorizeEndpoint,
 		TokenEndpoint:                     fmt.Sprintf("%s/v0/oauth/token", c.Issuer),
 		UserInfoEndpoint:                  fmt.Sprintf("%s/v0/openid/userinfo", c.Issuer),
 		JwksURI:                           fmt.Sprintf("%s/.well-known/jwks.json", c.Issuer),
